d586a08b989dce2c9fab13a511804e8e339e6846
eric <eric.rabelo21@gmail.com>
2023-06-05 14:36:42 -0300

    subindo entrega 2

---
+++ Entrega 2/trabalhoOO/src/trabalhoOO/Acesso.java
@@ -0,0 +1,5 @@
+package trabalhoOO;
+
+public class Acesso {
+
+}
---
+++ Entrega 2/trabalhoOO/src/trabalhoOO/Main.java
@@ -0,0 +1,10 @@
+package trabalhoOO;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
