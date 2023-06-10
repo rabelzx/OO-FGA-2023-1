fd9082c24ed56897bf9a280d92fcf65291e23585
João Victor de Carvalho Nobre <65377921+Gam13@users.noreply.github.com>
2023-06-10 14:24:39 -0300

    Classe de Horario

--- Entrega 2/src/trabalhoOO/Horario.java
+++ Entrega 2/src/trabalhoOO/Horario.java
@@ -3,4 +3,18 @@ package trabalhoOO;
 public class Horario {
     private int hora;
     private int minuto;
+
+
+    public Horario(){}
+    public Horario(int hora, int minuto){
+        this.hora = hora;
+        this.minuto = minuto;
+    }
+    void setHora(int hora){this.hora = hora;}
+    void setMinuto(int minuto){this.minuto = minuto;}
+
+    public String toString(){
+        System.out.println(hora+";"+minuto);
+        return null;
+    }
 }
