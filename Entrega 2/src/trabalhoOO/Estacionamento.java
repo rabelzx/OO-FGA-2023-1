ec6655965be3fbc5e201fa4a1b39e828faa1b917
João Victor de Carvalho Nobre <65377921+Gam13@users.noreply.github.com>
2023-06-10 11:18:51 -0300

    arquivos base

--- Entrega 2/src/trabalhoOO/Estacionamento.java
+++ Entrega 2/src/trabalhoOO/Estacionamento.java
@@ -3,7 +3,8 @@ package trabalhoOO;
 public class Estacionamento {
 	private int capacidade;
 	private float valCon;
-	
+	private Horario abrir;
+	private Horario fechar;
 	public Estacionamento() {}
 	
 
---
+++ Entrega 2/src/trabalhoOO/Horario.java
@@ -0,0 +1,6 @@
+package trabalhoOO;
+
+public class Horario {
+    private int hora;
+    private int minuto;
+}
