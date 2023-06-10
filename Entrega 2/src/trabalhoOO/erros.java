cb3bd6d970c02b73b6fea552b88e26eafafa56b2
Jagaima <davinobre.ik@gmail.com>
2023-06-10 16:38:05 -0300

    Criação da classe erros para colocar as exceções exigididas pelo professor

--- Entrega 2/src/trabalhoOO/erros.java
+++ Entrega 2/src/trabalhoOO/erros.java
@@ -1,1 +1,14 @@
+package trabalhoOO;
 
+public class erros (String[] args) {
+    int valorP1;
+    String strValorP1;
+    strValorP1 = JOptionPane.showInputDialog("");
+    if (strValorP1.length()==0) {
+        System.out.printf("");
+    }
+    else {
+        valorP1 = Integer.parseInt(ValorP1);
+        System.out.println(valorP1);
+    }
+}
