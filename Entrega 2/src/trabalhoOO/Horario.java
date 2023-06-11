2b5a554bb90885173a68e747b2e67be3a1dc38c1
João Victor de Carvalho Nobre <65377921+Gam13@users.noreply.github.com>
2023-06-11 11:15:02 -0300

    Ajustes para melhor funcionamento de Horario.java

--- Entrega 2/src/trabalhoOO/Horario.java
+++ Entrega 2/src/trabalhoOO/Horario.java
@@ -7,15 +7,22 @@ public class Horario {
 
     public Horario(){}
     public Horario(int hora, int minuto){
-        this.hora = hora;
-        this.minuto = minuto;
+        if (hora >= 24 || hora < 0){
+            System.out.printf("\na Hora inserida é invalida!!\n");
+        } else if (minuto >= 60 || minuto < 0) {
+            System.out.printf("\nO minuto inserido é invalido!!\n");
+        }else {
+            this.hora = hora;
+            this.minuto = minuto;
+            System.out.printf("Hora Registrada com Sucesso\n");
+        }
     }
+    //Função que une a hora com os minutos em uma string
+    public String unirHorario(){
+        String horario = hora+":"+minuto;
+        return horario;
+    }
+    //Sets
     void setHora(int hora){this.hora = hora;}
     void setMinuto(int minuto){this.minuto = minuto;}
-
-    //Função printa a Hora
-    public String toString(){
-        System.out.printf(hora+":"+minuto);
-        return null;
-    }
 }
