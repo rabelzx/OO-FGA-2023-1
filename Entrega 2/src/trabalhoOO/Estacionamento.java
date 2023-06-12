20a051b00cbf973fe5c41d315c8273adb8b318f6
João Victor de Carvalho Nobre <65377921+Gam13@users.noreply.github.com>
2023-06-12 16:15:28 -0300

    função que calcula o valor que retorna ao contratante

--- Entrega 2/src/trabalhoOO/Estacionamento.java
+++ Entrega 2/src/trabalhoOO/Estacionamento.java
@@ -2,13 +2,14 @@ package trabalhoOO;
 
 
 import javax.swing.*;
+import java.util.ArrayList;
 
 public class Estacionamento {
 	private int capacidade;
 	private float valCon;
 	private Horario abrir;
 	private Horario fechar;
-	private Acesso acessos[];
+	ArrayList<Acesso> acessos = new ArrayList<>();
 	private Valores valores;
 
 	private int quantidade_Acessos=0;
@@ -21,7 +22,7 @@ public class Estacionamento {
 		this.valCon = valCon;
 	}
 	public void cadastrarAcesso(String placa, Boolean evento, Boolean mensalista,Data data, Horario entrada, Horario saida) {
-		acessos[quantidade_Acessos] = new Acesso(placa,evento,mensalista,entrada,saida,data,valores,abrir,fechar);
+		acessos.set(quantidade_Acessos, new Acesso(placa, evento, mensalista, entrada, saida, data, valores, abrir, fechar));
 		quantidade_Acessos++;
 	}
 
@@ -29,24 +30,27 @@ public class Estacionamento {
 		float diurna = (diurnapercent/100), noturna = (noturnapercent/100);
 		valores = new Valores(fracao,hora_cheia,diurna,noturna,mensalidade,evento);
 	}
+	//Calcula o retorno do contratante
 	public float calcRetorno(int contratantePercent) {
 		int retorno = 0;
 		for (int i = 0; i < quantidade_Acessos; i++) {//Verífica todos os acessos
-			if (acessos[i].getChkMens()) {//Verifica se o acesso é de uma placa mensalista
-				for (int j = 0; j < i; j++) {//Varre o vetor até o ponto que estamos na verificação
-					if (acessos[i].getPlaca() == acessos[j].getPlaca()) { //Verifica se o carro já fez uma acesso no mês
-						i++; //caso ele tenha feito outro acesso no mês ele passa para o próximo no vetor
-					}
-					else {//Caso contrário ele adiciona o acesso mensal ao montante
-						retorno += acessos[i].calcPrice(abrir, fechar, valores);//Adiciona o que o carro pagou ao montante
-					}
 
+			if (acessos.get(i).getChkMens()) {//Verifica se o acesso é de uma placa mensalista
+				for (int j = 0; j < i; j++) {//Varre o vetor até o ponto que estamos na verificação
+
+					if (acessos.get(i).getPlaca() == acessos.get(j).getPlaca()) { //Verifica se o carro já fez uma acesso no mês
+						i++;} //caso ele tenha feito outro acesso no mês ele passa para o próximo no vetor
+
+					else {//Caso contrário ele adiciona o acesso mensal ao montante
+						retorno += acessos.get(i).calcPrice(abrir, fechar, valores);}//Adiciona o que o carro pagou ao montante
 				}
+
 			}
-			else{retorno += acessos[i].calcPrice(abrir, fechar, valores);}//Adiciona o que o carro pagou ao montante
+			else{retorno += acessos.get(i).calcPrice(abrir, fechar, valores);}//Adiciona o que o carro pagou ao montante
 		}
-		return retorno * valCon;//Retorna o quanto o contratante recebeu
-	}
+		return retorno * valCon;}//Retorna o quanto o contratante recebeu
+
+
 	//====================================================================================
 	//Sets e Gets
 	//====================================================================================
